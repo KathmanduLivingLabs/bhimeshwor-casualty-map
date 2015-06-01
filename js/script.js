4ca83d9c50995d185049799b92e2e2e5613e90b8
jediKnight <gautamxpratik@gmail.com>
2015-06-01 09:24:54 +0545

    Update script.js

--- js/script.js
+++ js/script.js
@@ -77,9 +77,9 @@ queue()
 
 function ready(error, districts_topo) {
   svg.append("g")
-      .attr("class", "vdcs")
+      .attr("class", "admindiv")
     .selectAll("path")
-      .data(topojson.feature(districts_topo, districts_topo.objects.vdcs).features)
+      .data(topojson.feature(districts_topo, districts_topo.objects.admindiv).features)
     .enter().append("path")
       .attr("class", function(d) { 
         return ramp(d,m)
@@ -91,13 +91,13 @@ function ready(error, districts_topo) {
 
 // Draw the state borders
   svg.append("path")
-      .datum(topojson.mesh(districts_topo, districts_topo.objects.vdcs, function(a, b) { 
+      .datum(topojson.mesh(districts_topo, districts_topo.objects.admindiv, function(a, b) { 
         return a !== b; }))
       .attr("class", "states")
       .attr("d", path)
 
     svg.append("path")
-      .datum(topojson.mesh(districts_topo, districts_topo.objects.vdcs, function(a, b) { 
+      .datum(topojson.mesh(districts_topo, districts_topo.objects.admindiv, function(a, b) { 
         return a == b; }))
       .attr("class", "country")
       .attr("d", path)      
@@ -204,7 +204,7 @@ function remover() {
 }
 
 function rebuild() {
-  d3.selectAll(".vdcs").remove();
+  d3.selectAll(".admindiv").remove();
   d3.selectAll(".country").remove();
   d3.selectAll(".states").remove();
   queue()
